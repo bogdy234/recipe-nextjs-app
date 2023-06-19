ff07c72d8bca98fb0d9f4f15147ee17005cab844
Bogdan Filimon <bogdanfilimon@Bogdans-MacBook-Pro.local>
2023-06-19 15:40:49 +0300

    fetch data

---
+++ app/interfaces/meal.ts
@@ -0,0 +1,9 @@
+export interface IMeal {
+  idMeal: string;
+  strMeal: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strYoutube: string;
+}
